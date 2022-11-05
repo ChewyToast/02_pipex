9688d5fc95803ed21636a0079e80ebe05b9c19f3
ChewyToast <moll.bruno.99@gmail.com>
2022-11-05 04:59:27 +0100

    input parsing maded

--- src/parts/first_part.c
+++ src/parts/first_part.c
@@ -5,15 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmoll-pe <bmoll-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/04 16:19:02 by bmoll-pe          #+#    #+#             */
-/*   Updated: 2022/11/05 04:05:33 by bmoll-pe         ###   ########.fr       */
+/*   Created: 2022/11/05 04:58:17 by bmoll-pe          #+#    #+#             */
+/*   Updated: 2022/11/05 04:58:23 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse_utils.h"
 #include "bmlib.h"
-
-static int	check_and_exec(char *file, t_pipex *pipex);
 
 int	parse_first_part(char *file, t_pipex *pipex)
 {
@@ -25,24 +23,16 @@ int	parse_first_part(char *file, t_pipex *pipex)
 		return (ft_pipex_error(NULL, "bash:",
 				" error creating first fork()", 0));
 	if (!pid)
-		check_and_exec(file, pipex);
+	{
+		if (!ft_check_file(file, R_OK))
+			exit (0);
+		if (!ft_check_cmd(pipex->cmds, pipex->path))
+			exit (0);
+	}
 	else
 	{
 		waitpid(pid, &status, 0);
 		return (WEXITSTATUS(status));
 	}
-	return (1);
-}
-
-static int	check_and_exec(char *file, t_pipex *pipex)
-{
-	if (!ft_check_file(file, R_OK))
-		exit (0);
-	if (!ft_check_cmd(pipex->cmds, pipex->path))
-		exit (0);
-	// if (!ft_open_and_pipe(file, pipex))
-	// 	exit (0);
-	// if (!ft_execve(pipex))
-	// 	exit (0);
 	return (1);
 }
