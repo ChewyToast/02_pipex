860d2cf605c4bb0a58d3ca6342e61cd31517c76b
Bruno Moll <brunomoll@Brunos-MacBook-Pro.local>
2022-11-16 02:37:55 +0100

    it works much better

--- src/pipex.c
+++ src/pipex.c
@@ -58,7 +58,7 @@ void	first_part(t_pipex *pip)
 	if (dup2(pip->utils->pipes[1], 1) < 0)
 		exit (error_msg(BSH, "dup2", BFD, clean_exit(pip, 1)));
 	close(pip->utils->pipes[0]);
-	close(0);
+	// close(0);
 	execve(*(pip->cmds->cmd), pip->cmds->cmd, pip->inputs->env);
 	clean_exit(pip, 1);
 	perror(NULL);
@@ -70,7 +70,7 @@ void	second_part(t_pipex *pip)
 	get_path(pip, "PATH=");
 	check_file(*(pip->inputs->argv + (pip->inputs->argc - 1)), W_OK, pip);
 	pip->inputs->argv += pip->inputs->argc - 4;
-	pip->inputs->outfd = open(*(pip->inputs->argv + 3), O_CREAT | O_RDWR, 0644);
+	pip->inputs->outfd = open(*(pip->inputs->argv + 3), O_CREAT | O_RDWR, 0666);
 	if (pip->inputs->inpfd < 0)
 	{
 		error_msg(BSH, *(pip->inputs->argv + 3), CNO, 1);
@@ -81,7 +81,7 @@ void	second_part(t_pipex *pip)
 		exit (error_msg(NULL, "bash", MKO, clean_exit(pip, 1)));
 	check_cmd(pip, pip->cmds);
 	if (dup2(pip->inputs->outfd, 1) < 0)
-		exit (error_msg(BSH, "dup2", BFD, clean_exit(pip, 1)));
+		exit (error_msg(BSH, "2", BFD, clean_exit(pip, 1)));
 	if (!pip->utils->error)
 		execve(*(pip->cmds->cmd), pip->cmds->cmd, pip->inputs->env);
 	exit (clean_exit(pip, 1));
